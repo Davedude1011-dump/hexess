3a51792ddbfd0ff93547da2391583c9131419292
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2023-10-20 10:40:33 +0100

    Add files via upload

--- function.js
+++ function.js
@@ -1,5 +1,6 @@
 function GenerateGameCode() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let GameName = "hexess"
     let randomString = '';
 
     for (let i = 0; i < 20; i++) {
@@ -8,13 +9,13 @@ function GenerateGameCode() {
     }
 
     console.log(window.location.href);
-    console.log(`${window.location.origin}/game/index.html?GameCode=${randomString}`);
+    console.log(`${window.location.origin}/${GameName}/game/index.html?GameCode=${randomString}`);
 
-    document.querySelector(".game-code").textContent = `${window.location.origin}/game/index.html?GameCode=${randomString}`
+    document.querySelector(".game-code").textContent = `${window.location.origin}/${GameName}/game/index.html?GameCode=${randomString}`
     document.querySelector(".overlay").style.display = "flex"
 
     document.querySelector(".join-game").addEventListener("click", function() {
-        window.open(`${window.location.origin}/game/index.html?GameCode=${randomString}`, "_self")
+        window.open(`${window.location.origin}/${GameName}/game/index.html?GameCode=${randomString}`, "_self")
     })
 }
 document.querySelector(".create-game").addEventListener("click", GenerateGameCode)
